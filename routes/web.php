a38f98caf7ce976cc73a260ee0005a90e5081554
user <songoci222@gmail.com>
2024-12-01 12:11:48 +0200

    admin structure changing

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
-Route::get('/admin', \App\Http\Controllers\AdminController::class);
+
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
